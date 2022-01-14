248cf8342524290fe47d6905ee4e16cee4acd857
Pragathi-pm <97746655+Pragathi-pm@users.noreply.github.com>
2022-01-14 20:37:38 +0530

    Create basic.c

---
+++ basic.c
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Pragathi-pm
+- 👀 I’m interested in learning computer science...
+- 🌱 I’m currently learning c language..
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me through email.
+
+<!---
+Pragathi-pm/Pragathi-pm is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
